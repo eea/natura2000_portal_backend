1fa0079d4b5a6b011b83b2bf160d3212a3390964
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2024-11-08 12:58:10 +0100

    Upgrade Info Services

--- natura2000_portal_back/Services/IInfoService.cs
+++ natura2000_portal_back/Services/IInfoService.cs
@@ -5,9 +5,9 @@ namespace natura2000_portal_back.Services
     public interface IInfoService
     {
         Task<List<ReleasesCatalog>> GetOfficialReleases();
-        Task<List<SitesParametered>> GetParameteredSites(long? releaseId, string? siteType, string? country, string? bioregion, string? siteCode, string? siteName, string? habitatCode, string? speciesCode);
-        Task<List<HabitatsParametered>> GetParameteredHabitats(long? releaseId, string? habitatGroup, string? country, string? bioregion, string? habitatCode, string? habitatName);
-        Task<List<SpeciesParametered>> GetParameteredSpecies(long? releaseId, string? speciesGroup, string? country, string? bioregion, string? speciesCode, string? speciesName);
+        Task<List<SitesParametered>> GetParameteredSites(long? releaseId, string? siteType, string? country, string? bioregion, string? site, string? habitat, string? species);
+        Task<List<HabitatsParametered>> GetParameteredHabitats(long? releaseId, string? habitatGroup, string? country, string? bioregion, string? habitat);
+        Task<List<SpeciesParametered>> GetParameteredSpecies(long? releaseId, string? speciesGroup, string? country, string? bioregion, string? species);
         Task<ReleaseCounters> GetLatestReleaseCounters();
     }
 }
